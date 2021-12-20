6b39df598b514ca43c822be1be030fcc3b93a2ad
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-12-20 14:35:06 -0300

    Add files via upload

---
+++ API/SenaiSpmedicalgroup.webApi/Controllers/SituacoesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiSpmedicalgroup.webApi.Controllers
+{
+    [Produces("application/Json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SituacoesController : ControllerBase
+    {
+    }
+}
---
+++ API/SenaiSpmedicalgroup.webApi/Interfaces/IMedicoRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SenaiSpmedicalgroup.webApi.Domains;
+using SenaiSpmedicalgroup.webApi.Repositories;
+
+namespace SenaiSpmedicalgroup.webApi.Interfaces
+{
+    interface IMedicoRepository
+    {
+        protected List<MedicoDomain> Listar();
+
+        protected MedicoDomain BuscarPorId(int id);
+
+        protected void Inserir(MedicoDomain medico);
+
+        protected void Deletar(int id);
+
+        protected void Atualizar(int id, MedicoDomain medicoAtualizado);
+    }
+}
