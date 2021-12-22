c72c1cdd819560d109b7f61f907642bed925f63b
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-12-22 16:16:35 -0300

    Add files via upload

---
+++ API/SenaiSpmedicalgroup.webApi/Domains/UsuarioDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiSpmedicalgroup.webApi.Domains
+{
+    public class UsuarioDomain
+    {
+        public int idUsuario { get; set; }
+
+        public string nomeUsuario { get; set; }
+
+        public string emailUsuario { get; set; }
+
+        public string senhaUsuario { get; set; }
+    }
+}
